26a0ca1977cd6fb6d4e9e441d801f08a85332f96
Renato Bezerra <renato-bgs@live.com>
2026-03-02 02:27:03 -0300

    feat: adiciona URLs de páginas de serviço ao sitemap e atualiza a data de última modificação.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.mariaclarasantos.adv.br',
-    generateRobotsTxt: true,
+  siteUrl: "https://www.mariaclarasantos.adv.br",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
 };
